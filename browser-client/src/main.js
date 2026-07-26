fe8273d2568782fb64de5e6cb521cdd9cdbcf7c0
Eriola <ogunsinaeriola@gmail.com>
2026-07-27 00:32:47 +0100

    feat: point dApp at live public relay

--- browser-client/src/main.js
+++ browser-client/src/main.js
@@ -4,7 +4,7 @@ const { Noir } = require('@noir-lang/noir_js');
 const circuit = require('../../zk_intent_circuit/target/zk_intent_circuit.json');
 const tf = require('@tensorflow/tfjs');
 const faceLandmarksDetection = require('@tensorflow-models/face-landmarks-detection');
-const RELAY_URL = 'http://localhost:3000';
+const RELAY_URL = 'https://oblivia-relay.onrender.com';
 
 let biometricCaptured = false;
 let biometricFeatures = null;
